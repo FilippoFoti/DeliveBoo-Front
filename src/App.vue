c904213f4350102cd1dfd668c01a8e1851380d8a
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-07 15:03:27 +0200

    fixed filter

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   data() {
     return {
       restaurants: [],
-      types:[],
+      types: [],
       selectedType: "all",
     }
   },
@@ -13,60 +13,61 @@ export default {
     this.getRestaurants();
     this.getTypes();
   },
-  methods: {    
+  methods: {
     getTypes() {
-      axios.get('http://localhost:8000/api/types').then(resp => {
-        this.types = resp.data.results;
-      })
+      axios.get('http://localhost:8000/api/types')
+        .then(resp => {
+          this.types = resp.data.results;
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
     getRestaurants() {
+      const params = {};
 
-      const params = {}
-
-      if(this.selectedType !== "all") {
+      if (this.selectedType !== "all") {
         params.type_id = this.selectedType;
       }
 
-      axios.get('http://localhost:8000/api/restaurants', { params }).then(resp => {
-        this.restaurants = resp.data.restaurants;
-      })
+      axios.get('http://localhost:8000/api/restaurants', { params })
+        .then(resp => {
+          this.restaurants = resp.data.restaurants;
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
-
-  }
-}
-
+  },
+  computed: {
+    filteredRestaurants() {
+      if (this.selectedType === "all") {
+        return this.restaurants;
+      } else {
+        return this.restaurants.filter(restaurant => restaurant.type_id === this.selectedType);
+      }
+    },
+  },
+};
 </script>
 
-<template>  
-
-
-  <!-- filtri -->
+<template>
   <div class="container">
-    <p>
+    <label for="type">Tipi di ristoranti</label>
+    <select v-model="selectedType" id="type" class="form-select" @change="getRestaurants">
+      <option value="all">Tutti</option>
+      <option :value="typeItem.id" v-for="typeItem in types" :key="typeItem.id">{{ typeItem.name }}</option>
+    </select>
 
-      <label for="type">Tipi di ristoranti</label>
-      <select v-model="selectedType"  id="type" class="form-select" @change="getRestaurants()">
-        <option value="all">tutte</option>
-        <option :value="typeItem.id" v-for="typeItem in types " :key="typeItem.id">{{ typeItem.name }}</option>
-      </select>
-
-    </p>
-
-  </div>
-
-  <div class="container">
-    <h1>lista ristoranti</h1>
+    <h1>Lista ristoranti</h1>
     <div class="row row-cols-4">
-      <div class="col" v-for="restaurant in restaurants" :key="restaurant.id">
+      <div class="col" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
         <div class="card">
-          <h3> {{ restaurant.name }}</h3>
+          <h3>{{ restaurant.name }}</h3>
         </div>
       </div>
     </div>
   </div>
-
-
-
 </template>
 
 <style lang="scss">
