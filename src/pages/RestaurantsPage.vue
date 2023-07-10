1630a38c6f6428723ad12a1809d30166586fb8fd
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-07-10 17:09:07 +0200

    Add: braintree-web dependencies

--- src/pages/RestaurantsPage.vue
+++ src/pages/RestaurantsPage.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
@@ -11,11 +11,14 @@ export default {
       selectedRestaurant: null,
       showRestaurantDetails: false,
       selectedRestaurantDetails: null,
-    }
+    };
   },
   computed: {
     cartTotal() {
-      const totalAmount = this.cart.reduce((total, item) => total + parseFloat(item.price * item.count), 0);
+      const totalAmount = this.cart.reduce(
+        (total, item) => total + parseFloat(item.price * item.count),
+        0
+      );
       return totalAmount.toFixed(2);
     },
   },
@@ -28,11 +31,12 @@ export default {
 
   methods: {
     getTypes() {
-      axios.get('http://localhost:8000/api/types')
-        .then(resp => {
+      axios
+        .get("http://localhost:8000/api/types")
+        .then((resp) => {
           this.types = resp.data.results;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     },
@@ -45,16 +49,17 @@ export default {
         params.type_id = this.selectedType;
       }
 
-      axios.get('http://localhost:8000/api/restaurants', { params })
-        .then(resp => {
+      axios
+        .get("http://localhost:8000/api/restaurants", { params })
+        .then((resp) => {
           this.restaurants = resp.data.results;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     },
     showMenu(restaurantId) {
-      const restaurant = this.restaurants.find(r => r.id === restaurantId);
+      const restaurant = this.restaurants.find((r) => r.id === restaurantId);
       if (restaurant) {
         this.selectedRestaurant = restaurant;
         this.showRestaurantDetails = false;
@@ -71,25 +76,30 @@ export default {
       this.selectedRestaurantDetails = null;
     },
     addToCart(dishe) {
-      const existingItem = this.cart.find(item => item.id === dishe.id);
+      const existingItem = this.cart.find((item) => item.id === dishe.id);
       if (existingItem) {
         existingItem.count++;
       } else {
         const newItem = { ...dishe, count: 1 };
         this.cart.push(newItem);
       }
-      if (this.selectedRestaurant && this.selectedRestaurant.id === dishe.restaurantId) {
-        this.selectedRestaurant.dishes = this.selectedRestaurant.dishes.map(d => {
-          if (d.id === dishe.id) {
-            return { ...d, count: existingItem ? existingItem.count : 1 };
+      if (
+        this.selectedRestaurant &&
+        this.selectedRestaurant.id === dishe.restaurantId
+      ) {
+        this.selectedRestaurant.dishes = this.selectedRestaurant.dishes.map(
+          (d) => {
+            if (d.id === dishe.id) {
+              return { ...d, count: existingItem ? existingItem.count : 1 };
+            }
+            return d;
           }
-          return d;
-        });
+        );
       }
-      this.saveCartToLocalStorage()
+      this.saveCartToLocalStorage();
     },
     removeFromCart(dishe) {
-      const index = this.cart.findIndex(cartItem => cartItem.id === dishe.id);
+      const index = this.cart.findIndex((cartItem) => cartItem.id === dishe.id);
       if (index !== -1) {
         const currentItem = this.cart[index];
 
@@ -99,41 +109,55 @@ export default {
           this.cart.splice(index, 1);
         }
 
-        if (this.selectedRestaurant && this.selectedRestaurant.id === dishe.restaurantId) {
-          this.selectedRestaurant.dishes = this.selectedRestaurant.dishes.map(d => {
-            if (d.id === dishe.id) {
-              return { ...d, count: currentItem.count };
+        if (
+          this.selectedRestaurant &&
+          this.selectedRestaurant.id === dishe.restaurantId
+        ) {
+          this.selectedRestaurant.dishes = this.selectedRestaurant.dishes.map(
+            (d) => {
+              if (d.id === dishe.id) {
+                return { ...d, count: currentItem.count };
+              }
+              return d;
             }
-            return d;
-          });
+          );
         }
       }
-      this.saveCartToLocalStorage()
+      this.saveCartToLocalStorage();
     },
     clearCart() {
       this.cart = [];
       this.saveCartToLocalStorage();
     },
     loadCartFromLocalStorage() {
-      const cartData = localStorage.getItem('cart');
+      const cartData = localStorage.getItem("cart");
       if (cartData) {
         this.cart = JSON.parse(cartData);
       }
     },
     saveCartToLocalStorage() {
-      localStorage.setItem('cart', JSON.stringify(this.cart));
+      localStorage.setItem("cart", JSON.stringify(this.cart));
     },
   },
 };
 </script>
 
 <template>
-  <div class="container" :class="{ 'popup-open': selectedRestaurant || showRestaurantDetails }">
+  <div
+    class="container"
+    :class="{ 'popup-open': selectedRestaurant || showRestaurantDetails }"
+  >
     <label>Tipi di ristoranti</label>
     <div class="d-flex gap-3">
       <div class="form-check" v-for="typeItem in types" :key="typeItem.id">
-        <input type="checkbox" :id="'type_' + typeItem.id" :value="typeItem.id" v-model="selectedType"
-          @change="getRestaurants" class="form-check-input" />
+        <input
+          type="checkbox"
+          :id="'type_' + typeItem.id"
+          :value="typeItem.id"
+          v-model="selectedType"
+          @change="getRestaurants"
+          class="form-check-input"
+        />
         <label :for="'type_' + typeItem.id" class="form-check-label">
           {{ typeItem.name }}
         </label>
@@ -142,11 +166,19 @@ export default {
 
     <h1>Lista ristoranti</h1>
     <div class="row row-cols-4">
-      <div class="col mb-3" v-for="restaurant in restaurants" :key="restaurant.id">
+      <div
+        class="col mb-3"
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
         <div class="card">
           <h3>{{ restaurant.name }}</h3>
-          <button @click="showMenu(restaurant.id)" class="btn btn-primary mb-2">Mostra menu</button>
-          <button @click="showDetails(restaurant)" class="btn btn-primary">Dettagli</button>
+          <button @click="showMenu(restaurant.id)" class="btn btn-primary mb-2">
+            Mostra menu
+          </button>
+          <button @click="showDetails(restaurant)" class="btn btn-primary">
+            Dettagli
+          </button>
         </div>
       </div>
     </div>
@@ -156,44 +188,60 @@ export default {
         <div class="popup-content p-5">
           <h2>{{ selectedRestaurant.name }} - Menu</h2>
           <ul class="px-0">
-            <li v-for="dishe in selectedRestaurant.dishes" :key="dishe.id" class="list-unstyled">
+            <li
+              v-for="dishe in selectedRestaurant.dishes"
+              :key="dishe.id"
+              class="list-unstyled"
+            >
               <div class="dishe-item">
                 <div class="dishe-image">
-                  <img class="mw-100" :src="dishe.image" alt="Dishe Image">
+                  <img class="mw-100" :src="dishe.image" alt="Dishe Image" />
                 </div>
                 <div class="dishe-details">
                   <h4>{{ dishe.name }}</h4>
                   <p>{{ dishe.description }}</p>
                   <p>{{ dishe.price }}</p>
                   <div v-if="dishe.count > 0">
-                    <button @click="removeFromCart(dishe)" class="btn btn-primary">-</button>
+                    <button
+                      @click="removeFromCart(dishe)"
+                      class="btn btn-primary"
+                    >
+                      -
+                    </button>
                     <span class="mx-2">{{ dishe.count }}</span>
                   </div>
-                  <button @click="addToCart(dishe)" class="btn btn-primary">Aggiungi al carrello</button>
+                  <button @click="addToCart(dishe)" class="btn btn-primary">
+                    Aggiungi al carrello
+                  </button>
                 </div>
               </div>
             </li>
           </ul>
-          <button @click="hideMenu" class="btn btn-primary close-button">X</button>
+          <button @click="hideMenu" class="btn btn-primary close-button">
+            X
+          </button>
         </div>
       </div>
     </div>
-
-
 
     <div v-if="showRestaurantDetails && selectedRestaurantDetails">
       <div class="popup">
         <div class="popup-content p-5">
           <h2>{{ selectedRestaurantDetails.name }} - Dettagli</h2>
-          <img class="mw-100" :src="selectedRestaurantDetails.image" alt="Restaurant Image">
+          <img
+            class="mw-100"
+            :src="selectedRestaurantDetails.image"
+            alt="Restaurant Image"
+          />
           <p>Indirizzo: {{ selectedRestaurantDetails.address }}</p>
           <p>Telefono: {{ selectedRestaurantDetails.phone }}</p>
           <p>P. Iva: {{ selectedRestaurantDetails.vat_number }}</p>
-          <button @click="hideMenu" class="btn btn-primary close-button">X</button>
+          <button @click="hideMenu" class="btn btn-primary close-button">
+            X
+          </button>
         </div>
       </div>
     </div>
-
 
     <div v-if="cart.length > 0">
       <h2>Carrello</h2>
@@ -203,23 +251,25 @@ export default {
             <h4>{{ item.name }}</h4>
             <p>€ {{ item.price }}</p>
             <div>
-              <button @click="removeFromCart(item)" class="btn btn-primary">-</button>
+              <button @click="removeFromCart(item)" class="btn btn-primary">
+                -
+              </button>
               <span class="mx-2">{{ item.count }}</span>
-              <button @click="addToCart(item)" class="btn btn-primary">+</button>
+              <button @click="addToCart(item)" class="btn btn-primary">
+                +
+              </button>
             </div>
           </div>
         </li>
       </ul>
       <div>
         <button @click="clearCart" class="btn btn-primary mb-2">Svuota</button>
+        <router-link to="payment"> Procedi al pagamento </router-link>
       </div>
       <h3 v-if="cart.length > 0">Totale: € {{ cartTotal }}</h3>
     </div>
-
-
   </div>
 </template>
-
 
 <style lang="scss">
 @use "../styles/general.scss" as *;
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.vue";
 import RestaurantsPage from "./pages/RestaurantsPage.vue";
 import AboutPage from "./pages/AboutPage.vue";
 import ContactPage from "./pages/ContactPage.vue";
+import PaymentPage from "./pages/PaymentPage.vue";
 // import SingleProjectPage from "./pages/SingleProjectPage.vue";
 
 
@@ -28,6 +29,11 @@ const router = createRouter({
             path: "/contact",
             name: "contact",
             component: ContactPage,
+        },
+        {
+            path: "/payment",
+            name: "payment",
+            component: PaymentPage,
         },
         // {
         //     path: "/projects/:slug",
