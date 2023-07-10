84865706301b020e476e7d943bb2794b96057d55
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-07-10 17:08:06 +0200

    Add: PaymentPage, axios call

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -1,31 +1,25 @@
-<script></script>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      clientToken: [],
+    };
+  },
+
+  mounted() {
+    axios.get("http://localhost:8000/api/generate_token").then((resp) => {
+      this.clientToken = resp.data.token;
+      console.log(this.clientToken);
+    });
+  }
+};
+</script>
+
 <template>
-  <head>
-    <meta charset="utf-8" />
-    <script src=",[object Object],"></script>
-  </head>
-
-  <body>
-    <!-- Step one: add an empty container to your page -->
-    <div id="dropin-container">
-
-    </div>
-
-    <script type="text/javascript">
-      // Step two: create a dropin instance using that container (or a string
-      //   that functions as a query selector such as '#dropin-container')
-      braintree.dropin.create({
-        authorization: clientToken,
-        container: document.getElementById('dropin-container'),
-        // ...plus remaining configuration
-      }, (error, dropinInstance) => {
-        // Use 'dropinInstance' here
-        // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
-      });
-
-    </script>
-  </body>
+  
 </template>
 <style scoped lang="scss">
-@use "../styles/general.scss" as *
+@use "../styles/general.scss" as *;
 </style>
