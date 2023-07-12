e2b158c886f002233506cde0c7b12e5e903ae26e
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-07-12 20:53:03 +0200

    Add: se visibilità è no, non stampa

--- src/pages/RestaurantMenuPage.vue
+++ src/pages/RestaurantMenuPage.vue
@@ -110,23 +110,26 @@ export default {
 
 <template>
     <div class="container">
-        <div v-for="restaurant in  restaurants " :key="restaurant.id">
+        <div v-for="restaurant in restaurants" :key="restaurant.id">
             <h3 id="ristorante" v-if="restaurant.id === selectedRestaurantId">Menu: {{ restaurant.name }}</h3>
             <div class="row row-cols-4">
-                <div v-for="dishe in  restaurant.dishes " :key="dishe.id" v-if="restaurant.id === selectedRestaurantId"
-                    class="col">
-                    <div class="card h-100">
-                        <img :src="state.imagePath(dishe.image)" class="card-img-topx" alt="...">
-                        <div class="card-body">
-                            <h4>{{ dishe.name }}</h4>
-                            <p>{{ dishe.description }}</p>
-                            <!-- id del ristorante nel piatto deve essere uguale al selectedRestauarntId -->
-                            <div></div>
-                            <button @click="addToCart(dishe)" class="btn btn-primary my-3"
-                                :disabled="!isSameRestaurantInCart(dishe.selectedRestaurantId)">Aggiungi al
-                                carrello</button>
+                <div v-if="restaurant.id === selectedRestaurantId">
+                    <div v-for="dishe in restaurant.dishes" :key="dishe.id" class="col mb-5">
+                        <div v-if="dishe.visibility == 1">
+                            <div class="card h-100">
+                                <img :src="state.imagePath(dishe.image)" class="card-img-topx" alt="...">
+                                <div class="card-body">
+                                    <h4>{{ dishe.name }}</h4>
+                                    <p>{{ dishe.description }}</p>
+                                    <p>{{ dishe.price }} €</p>
+                                    <button @click="addToCart(dishe)" class="btn btn-primary my-3"
+                                        :disabled="!isSameRestaurantInCart(dishe.selectedRestaurantId)">Aggiungi al
+                                        carrello</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -162,7 +165,7 @@ export default {
 @use "../styles/general.scss" as *;
 
 #ristorante {
-    padding-top: 90px;
+    padding-top: 100px;
 }
 
 .cart-container {
