da9373b26d51236064bc27af40677d23998903d7
FilippoFoti <filippofoti7@gmail.com>
2023-07-14 15:56:59 +0200

    Edit_paymentPage

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -125,54 +125,163 @@ export default {
 
 <template>
   <div class="container">
-    <h3 class="cart">Il tuo carrello</h3>
-    <ul>
-      <li v-for="item in cartArray" :key="item.id">
-        {{ item.name }} x {{ item.count }} - {{ item.count * item.price }} €
-      </li>
-    </ul>
-    <h5>Totale: {{ calculateTotal() }} €</h5>
-    <form>
-      <h3>Inserisci i tuoi dati</h3>
-      <div class="mb-3">
-        <label class="form-label">Nome</label>
-        <input type="text" class="form-control" v-model="name" :class="{'is-invalid': !isNameValid}">
-        <div v-if="!isNameValid" class="invalid-feedback">Inserisci un nome valido, senza numeri o caratteri speciali.</div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Cognome</label>
-        <input type="text" class="form-control" v-model="surname" :class="{'is-invalid': !isSurnameValid}">
-        <div v-if="!isSurnameValid" class="invalid-feedback">Inserisci un nome valido, senza numeri o caratteri speciali.</div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">E-mail</label>
-        <input type="email" class="form-control" v-model="email" :class="{'is-invalid': !isEmailValid}">
-        <div v-if="!isEmailValid" class="invalid-feedback">Inserisci un'e-mail valida (example@domain.com).</div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Telefono cellulare</label>
-        <input type="text" class="form-control" v-model="phone" :class="{'is-invalid': !isPhoneValid}" maxlength="10">
-        <div v-if="!isPhoneValid" class="invalid-feedback">Inserisci un numero di telefono valido (10 cifre).</div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Indirizzo</label>
-        <input type="text" class="form-control" v-model="address" :class="{'is-invalid': !isAddressValid}">
-        <div v-if="!isAddressValid" class="invalid-feedback">Inserisci un indirizzo valido.</div>
+    <h1 class="text-center mb-4 fw-bold">Pagamento</h1>
+    <div class="row">
+      <div class="col-8">
+        <div class="card p-4 shadow mb-5">
+          <form class="p-4">
+            <h2 class="fw-bold text-center mb-4">Inserisci i tuoi dati</h2>
+            <div class="container">
+              <div class="row row-cols-2">
+                <div class="col">
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Nome</label>
+                    <input type="text" class="form-control" v-model="name" :class="{ 'is-invalid': !isNameValid }">
+                    <div v-if="!isNameValid" class="invalid-feedback">Inserisci un nome valido, senza numeri o caratteri
+                      speciali.
+                    </div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">E-mail</label>
+                    <input type="email" class="form-control" v-model="email" :class="{ 'is-invalid': !isEmailValid }">
+                    <div v-if="!isEmailValid" class="invalid-feedback">Inserisci un'e-mail valida (example@domain.com).
+                    </div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Indirizzo</label>
+                    <input type="text" class="form-control" v-model="address" :class="{ 'is-invalid': !isAddressValid }">
+                    <div v-if="!isAddressValid" class="invalid-feedback">Inserisci un indirizzo valido.</div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Data di scadenza</label>
+                    <div id="expiration-date" class="form-control"></div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Cognome</label>
+                    <input type="text" class="form-control" v-model="surname" :class="{ 'is-invalid': !isSurnameValid }">
+                    <div v-if="!isSurnameValid" class="invalid-feedback">Inserisci un nome valido, senza numeri o
+                      caratteri
+                      speciali.</div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Telefono cellulare</label>
+                    <input type="text" class="form-control" v-model="phone" :class="{ 'is-invalid': !isPhoneValid }"
+                      maxlength="10">
+                    <div v-if="!isPhoneValid" class="invalid-feedback">Inserisci un numero di telefono valido (10 cifre).
+                    </div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">Numero della carta</label>
+                    <div id="credit-card-number" class="form-control"></div>
+                  </div>
+                  <div class="mb-2">
+                    <label class="form-label fw-bold">CVV</label>
+                    <div id="cvv" class="form-control mb-3"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="text-center mt-3">
+              <button type="submit" class="btn fw-bold" @click.prevent="submitForm()">Paga Ora</button>
+            </div>
+          </form>
+        </div>
       </div>
 
-
-      <label class="form-label">Numero della carta</label>
-      <div id="credit-card-number" class="form-control"></div>
-      <label class="form-label mt-3">Data di scadenza</label>
-      <div id="expiration-date" class="form-control"></div>
-      <label class="form-label mt-3">CVV</label>
-      <div id="cvv" class="form-control mb-3"></div>
-      <button type="submit" class="btn btn-primary my-3" @click.prevent="submitForm()">Paga Ora</button>
-    </form>
+      <div class="col-4">
+        <div class="card p-4 shadow mb-5">
+          <div class="card-body p-4">
+            <h2 class="text-center fw-bold mb-4">Riepilogo ordine</h2>
+            <ul v-for="item in cartArray" :key="item.id">
+              <li>{{ item.name }}</li>
+              <li class="unstyled">Quantità: <span class="fw-bold">x {{ item.count }}</span></li>
+              <li class="unstyled">Prezzo: <span class="fw-bold">{{ item.count * item.price }}€</span></li>
+            </ul>
+            <h3 class="text-end mt-5">Prezzo totale: <span class="fw-bold tot">{{ calculateTotal() }}€</span>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
+
+.container {
+  color: #03071E;
+}
+
+h1 {
+  padding-top: 110px;
+  color: #03071E;
+}
+
+.card {
+  border: none;
+  border-radius: 5px;
+
+  form,
+  .card-body {
+    border: 1px dashed #F2C802;
+    border-radius: 5px;
+
+    h2 {
+      font-size: 1.8rem;
+      color: #03071E;
+    }
+
+    ul {
+      color: #03071E;
+    }
+    .unstyled {
+      list-style-type: none;
+    }
+
+    h3 {
+      font-size: 1.3rem;
+    }
+
+    .tot {
+      color: #FAA307;
+    }
+
+    input,
+    #credit-card-number,
+    #expiration-date,
+    #cvv {
+      border: none;
+      border-radius: 5px;
+      background-color: #F9FAFC;
+      padding: 10px;
+    }
+
+    .form-control:focus {
+      border: 2px solid #F2C802;
+      border-color: #F2C802;
+      box-shadow: none;
+      color: #03071E;
+    }
+
+    .btn {
+      padding: 5px 10px;
+      background-color: #F2C802;
+      border: 1px solid #FAA307;
+      border-radius: 10px;
+      color: #03071E;
+
+      &:hover {
+        background-color: #FAA307;
+        transform: scale(1.1);
+        box-shadow: 0 10px 15px rgba(0, 0, 0, 0.5);
+      }
+
+    }
+  }
+}
+
 
 .is-invalid {
   border-color: red;
@@ -183,13 +292,10 @@ export default {
   font-size: 12px;
   margin-top: 5px;
 }
-.cart {
-  padding-top: 100px;
-}
 
 #credit-card-number,
 #expiration-date,
 #cvv {
-  height: 38px;
+  height: 44px;
 }
 </style>
